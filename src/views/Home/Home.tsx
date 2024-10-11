78c97d5e583a0547542d7b9f9f8738f15a19e38f
Ntsakelo Arnold <Ntsakeloarnold00@gmail.com>
2024-10-12 01:40:51 +0200

    hero banner section added

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,39 @@
 import Nav from "../../components/Nav";
-
-
+import Grid from '@mui/material/Grid2';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import './style.css'
 const Home = () => {
     return(
         <div>
             <Nav />
+            <Grid container>
+                <Grid size={{xs:12}}>
+                   <div className="hero-banner">
+                       <div>
+                       <p className="main-text hero-text">Find the right person to do the job right, at the right time</p>
+                       <ThemeProvider theme={createTheme({palette:{primary:{main:"#fff"},secondary:{main:"#fff"}},components:{MuiTextField:{styleOverrides:{root:{'--TextField-brandBorderHoverColor': '#fff'}}}}})}>
+                       <TextField 
+                       id="outlined-basic"  
+                       sx={{backgroundColor:'#fff',borderRadius:'5px',outline:'none',width: '50%',mt:'20px'}}
+                        slotProps={{
+                            input:{
+                                endAdornment:<InputAdornment position="end"><SearchIcon id="search-icon"/></InputAdornment>
+                            }
+                        }}
+                       />
+                       </ThemeProvider>  
+                       </div>
+                   </div>
+                </Grid>
+                <Grid size={{xs:12}}>
+                    <div className="service-item">
+                      
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     )
 }
